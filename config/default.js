c7ff1394005070a130f2932af88aaf173d077c7c
darya <kokorina.darya1509@gmail.com>
2019-02-01 15:03:58 +0300

    Merge branch 'feature/routes_and_controllers' of https://github.com/Darsha1509/adverts-backend into feature/routes_and_controllers
    # Conflicts:
    #	routes/users.js

--- config/default.js
+++ config/default.js
@@ -3,4 +3,5 @@
 module.exports = {
   host: '$HOST',
   port: '$PORT',
+  mongoDB: 'mongodb://dasha:a111111@ds117956.mlab.com:17956/mongodbfirst',
 };
--- server.js
+++ server.js
@@ -2,10 +2,15 @@
 
 const express = require('express');
 const config = require('getconfig');
+const mongoose = require('mongoose');
 
 const app = express();
 const users = require('./routes/users');
 const adverts = require('./routes/adverts');
+
+mongoose.connect(config.mongoDB, {useNewUrlParser: true});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connectionDB error:'));
 
 app.use(users);
 app.use(adverts);
