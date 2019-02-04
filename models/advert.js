e71d93bcec624fce90c7eb1b4475f69a1e6a4551
darya <kokorina.darya1509@gmail.com>
2019-02-04 11:52:27 +0300

    add models

---
+++ models/advert.js
@@ -0,0 +1,68 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const advertModelSchema = new Schema(
+  {
+    title: {
+      type: String,
+      min: 10,
+      max: 50,
+      required: true,
+      validate: {
+        validator(v) {
+          return /^[a-zA-Z0-9.-]*$/.test(v);
+        },
+        message: props => `${props.value} is not a valid title!`,
+      },
+    },
+    description: {
+      type: String,
+      validate: {
+        validator(v) {
+          return /^[a-zA-Z0-9.-]*$/.test(v);
+        },
+        message: props => `${props.value} is not a valid description!`,
+      },
+    },
+    category: {
+      type: String,
+      enum: [
+        'The property',
+        'Auto & transport',
+        'Equipment',
+        'Fashion & Style',
+        'Everything for children & mothers',
+        'Housing',
+        'Repair & construction',
+        'Garden',
+        'Hobby, sport & tourism',
+        'Wedding, bussiness & study',
+        'Services',
+        'Other',
+      ],
+    },
+    price: {
+      type: Number,
+      required: true,
+      validate: {
+        validator(v) {
+          return v > 0;
+        },
+        message: props => `${props.value} is not a valid price!`,
+      },
+    },
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  },
+  {
+    timestamp: {
+      createdAt: 'created_at',
+    },
+  }
+);
+
+// Export model.
+module.exports = mongoose.model('Advert', advertModelSchema);
--- server.js
+++ server.js
@@ -4,14 +4,22 @@ const express = require('express');
 const config = require('getconfig');
 const mongoose = require('mongoose');
 
+const User = require('./models/user');
+const Advert = require('./models/advert');
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connectionDB error:'));
+
 const app = express();
 const users = require('./routes/users');
 const adverts = require('./routes/adverts');
 
-mongoose.connect(config.mongoDB, {useNewUrlParser: true});
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connectionDB error:'));
-
 app.use(users);
 app.use(adverts);
-app.listen(config.port);
+
+mongoose.connect(config.mongoDB, { useNewUrlParser: true }, () => {
+  console.log('db connected');
+  app.listen(config.port, config.host, () => {
+    console.log(`Server running at http://${config.host}:${config.port}/`);
+  });
+});
